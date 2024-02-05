50216d823dafbadad006bdbdbf9ae03f588ceb3c
jenishbabariya22 <jenishbabariya58@gmail.com>
2024-02-05 17:47:05 +0530

    with-space-patten

---
+++ program-12.9.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+main(){
+	
+	int i,j,s;
+	
+	
+	for(i=5;i>=1;i--)
+{
+      for(j=1;j<=i;j++){
+	   printf("%d",j);
+
+
+      }
+      for(s=4;s>=i;s--){
+	printf("  ");
+
+      }
+ printf("\n");
+}
+
+	for(i=2;i<=5;i++)
+{
+      for(j=1;j<=i;j++){
+	   printf("%d",j);
+
+
+      }
+      for(s=4;s>=i;s--){
+	printf("  ");
+
+      }
+ printf("\n");
+}
+	
+}
